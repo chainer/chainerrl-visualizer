1457d2ecf46585e6378f0f5e901d2d24bf0da76c
sykwer <sykwer@gmail.com>
2018-08-15 15:00:28 +0900

    Add view(

--- appv0/frontend/src/containers/DetailContainer.jsx
+++ appv0/frontend/src/containers/DetailContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Container, Row, Col, Card, CardBody, CardTitle, CardText, InputGroup, Input, Button,
+  Container, Row, Col, Card, CardBody, CardTitle, CardText, InputGroup, Input, InputGroupAddon, Button,
 } from 'reactstrap';
 
 /* eslint-disable react/prefer-stateless-function */
@@ -8,48 +8,64 @@ import {
 export default class DetailContainer extends React.Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Col>
-            <Card>
-              <CardBody>
-                <CardTitle>Summary</CardTitle>
-                <CardText>Summary content</CardText>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <CardBody>
-                <CardTitle>Args</CardTitle>
-                <CardText>Args content</CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Card>
-              <CardBody>
-                <CardTitle>Rollout 1 episode</CardTitle>
-                <Container>
-                  <Row>
-                    <Col>
-                      <InputGroup>
-                        <Input placeholder="seed" />
-                      </InputGroup>
-                      <Button>Rollout</Button>
-                    </Col>
-                    <Col>
-                      create saliency map
-                    </Col>
-                  </Row>
-                </Container>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <div>
+        <br />
+        <Container>
+          <Row>
+            <Col>
+              <Card>
+                <CardBody>
+                  <CardTitle>Summary</CardTitle>
+                  <CardText>Summary content</CardText>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <CardBody>
+                  <CardTitle>Args</CardTitle>
+                  <CardText>Args content</CardText>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col>
+              <Card>
+                <CardBody>
+                  <CardTitle>Rollout 1 episode</CardTitle>
+                  <Container>
+                    <Row>
+                      <Col>
+                        <InputGroup>
+                          <InputGroupAddon addonType="prepend">result path</InputGroupAddon>
+                          <Input />
+                        </InputGroup>
+                        <br />
+                        <InputGroup>
+                          <InputGroupAddon addonType="prepend">model name</InputGroupAddon>
+                          <Input />
+                        </InputGroup>
+                        <br />
+                        <InputGroup>
+                          <InputGroupAddon addonType="prepend">seed</InputGroupAddon>
+                          <Input />
+                        </InputGroup>
+                        <br />
+                        <Button>Rollout</Button>
+                      </Col>
+                      <Col>
+                        create saliency map
+                      </Col>
+                    </Row>
+                  </Container>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
