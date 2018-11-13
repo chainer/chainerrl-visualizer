6273972d0d8a62c5fccac944dbd665c1418a710e
sykwer <sykwer@gmail.com>
2018-11-13 17:57:38 +0900

    Add empty components to valuePane

--- frontend/src/containers/ValuesPaneContainer.jsx
+++ frontend/src/containers/ValuesPaneContainer.jsx
@@ -9,18 +9,27 @@ import DiscreteStochasticActionsContainer from './DiscreteStochasticActionsConta
 import {
   mapAgentProfileToValuesPaneList,
   DISCRETE_ACTION_VALUE_PANE,
+  QUADRATIC_ACTION_VALUE_PANE,
   GAUSSIAN_DISTRIBUTION_PANE,
   SOFTMAX_DISTRIBUTION_PANE,
+  MELLOWMAX_DISTRIBUTION_PANE,
+  CONTINUOUS_DETERMINISTIC_DISTRIBUTION_PANE,
 } from '../settings/agent';
 
 const ValuesPaneContainer = ({ valuesPaneName }) => {
   switch (valuesPaneName) {
     case DISCRETE_ACTION_VALUE_PANE:
       return <DiscreteQvaluesContainer />;
+    case QUADRATIC_ACTION_VALUE_PANE:
+      return <div />; // TODO: implement QuadraticActionValuePane
     case GAUSSIAN_DISTRIBUTION_PANE:
       return <ContinuousStochasticActionsContainer />;
     case SOFTMAX_DISTRIBUTION_PANE:
       return <DiscreteStochasticActionsContainer />;
+    case MELLOWMAX_DISTRIBUTION_PANE:
+      return <div />; // TODO: implement MellowmaxDistributionPane
+    case CONTINUOUS_DETERMINISTIC_DISTRIBUTION_PANE:
+      return <div />; // TODO: implement ContinuousDeterministicDistributionPane
     default:
       return <div />;
   }
