a96b54c130598568b6ce99a99fc7f7fa2a9d621b
sykwer <sykwer@gmail.com>
2018-11-19 18:40:03 +0900

    Move polling management to MainPageContainer

--- frontend/src/containers/MainPageContainer.jsx
+++ frontend/src/containers/MainPageContainer.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { startFetchAgentProfile } from '../actions';
+import {
+  startFetchAgentProfile,
+  startFetchLatestLogInfo,
+  startFetchServerState,
+  startFetchLog,
+} from '../actions';
 import Header from '../components/Header';
 import MainPageBody from '../components/MainPageBody';
+import { startPolling, stopPolling } from '../utils/polling';
+
+const path = require('path');
+
+const POLLING_INTERVAL_MS = 3000;
+
+/* eslint-disable react/destructuring-assignment */
 
 class MainPageContainer extends React.Component {
   componentDidMount() {
-    this.props.startFetchAgentProfile(); // eslint-disable-line react/destructuring-assignment
+    this.props.startFetchAgentProfile();
+    this.props.startFetchLatestLogInfo();
+
+    this.logPollingTimer = startPolling(POLLING_INTERVAL_MS, this.props.startFetchLog, this.props.rolloutId);
+    this.serverStatePollingTimer = startPolling(POLLING_INTERVAL_MS, this.props.startFetchServerState);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.rolloutId !== prevProps.rolloutId) {
+      stopPolling(this.logPollingTimer);
+      this.logPollingTimer = startPolling(POLLING_INTERVAL_MS, this.props.startFetchLog, this.props.rolloutId);
+    }
+  }
+
+  componentWillUnmount() {
+    stopPolling(this.logPollingTimer);
+    stopPolling(this.serverStatePollingTimer);
   }
 
   render() {
@@ -22,9 +51,20 @@ class MainPageContainer extends React.Component {
 }
 
 MainPageContainer.propTypes = {
+  rolloutId: PropTypes.string.isRequired,
   startFetchAgentProfile: PropTypes.func.isRequired,
+  startFetchLatestLogInfo: PropTypes.func.isRequired,
+  startFetchServerState: PropTypes.func.isRequired,
+  startFetchLog: PropTypes.func.isRequired,
 };
 
-export default connect(null, {
+const mapStateToProps = (state) => ({
+  rolloutId: path.basename(state.log.rolloutPath),
+});
+
+export default connect(mapStateToProps, {
   startFetchAgentProfile,
+  startFetchLatestLogInfo,
+  startFetchServerState,
+  startFetchLog,
 })(MainPageContainer);
