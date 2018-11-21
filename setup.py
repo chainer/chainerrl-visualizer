4ab86f7762f3221d34da66f194c3df41b2e3ad63
ofk <ofkjpn+github@gmail.com>
2018-11-21 12:07:34 +0900

    Fix indent

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     dependencies = f.read().splitlines()
 
-setup(name="chainerrlui",
-      version="0.1",
-      packages=find_packages(exclude=("tests", "docs")),
-      install_requires=dependencies,
-      include_package_data=True,
-      package_data={
-          "chainerrlui": {
-              "templates/*",
-              "static/**/*",
-          }
-      },
-      author="sykwer",
-      author_email="sykwer@gmail.com",
-      description="UI tool for chainerrl",
-      )
+setup(
+    name="chainerrlui",
+    version="0.1",
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=dependencies,
+    include_package_data=True,
+    package_data={
+        "chainerrlui": {
+            "templates/*",
+            "static/**/*",
+        }
+    },
+    author="sykwer",
+    author_email="sykwer@gmail.com",
+    description="UI tool for chainerrl",
+)
